598a496162c991819530d7a41fe24b0745421b31
Tu Nombre <tuemail@example.com>
2026-01-08 23:14:24 +0100

    coherencia en los nombres con MainActivity

--- app/src/main/java/com/example/roomgatos/AppDatabase.kt
+++ app/src/main/java/com/example/roomgatos/AppDatabase.kt
@@ -1,16 +1,9 @@
-package gz.dam.catroomapp.data.database
+package com.example.roomgatos
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
-import com.example.roomgatos.GatosEntity
-import gz.dam.catroomapp.data.dao.CatDao
-import gz.dam.catroomapp.data.entity.CatEntity
 
-@Database(
-    entities = [GatosEntity::class],
-    version = 1,
-    exportSchema = false
-)
+@Database(entities = [GatosEntity::class], version = 1, exportSchema = false)
 abstract class AppDatabase : RoomDatabase() {
-    abstract fun catDao(): GatosDao
+    abstract fun GatosDao(): GatosDao
 }
